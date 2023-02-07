d750a6714bc73b0e8a93862e647c5e8754f7965e
FALEICA <fabio.leite.cardoso@gmail.com>
2023-02-07 08:55:10 -0300

    Implementando a View de Seller manualmente

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 using System.Configuration;
+using System.Drawing.Printing;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
+
+
 
 
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
@@ -16,10 +25,14 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 
 
 
-
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
+
+
+
 
 var app = builder.Build();
 
@@ -30,6 +43,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
